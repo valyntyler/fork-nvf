cef9e478a5117edb66dcc1ff0adeecb2dbec88c1
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-03 20:01:56 +0200

    temp: imported my own stuff

--- modules/plugins/languages/gdscript.nix
+++ modules/plugins/languages/gdscript.nix
@@ -1,2 +1,10 @@
-{
+{pkgs, ...}: {
+  config.vim.extraPlugins = {
+    vim-godot = {
+      package = pkgs.vimPlugins.vim-godot;
+    };
+  };
+  config.vim.luaConfigRC.vim-godot = ''
+    require'lspconfig'.gdscript.setup{capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())}
+  '';
 }
